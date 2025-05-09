52deee94ef57b6e373af13eec28c9375d734f67c
JHow <jhowliu@gmail.com>
2025-05-09 17:09:09 +1000

    fix: remove useless var

--- api/services/base_service.py
+++ api/services/base_service.py
@@ -3,7 +3,6 @@ from utils.mysql_init import MySQLManager
 class BaseService:
     def __init__(self):
         self.mysql = MySQLManager.get_mysql()
-        self.db = self.mysql.connection
 
     def get_cursor(self):
         return self.mysql.connection.cursor() 
