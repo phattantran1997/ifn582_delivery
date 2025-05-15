419c0dc6afc032f85f31a5194a08c391cbd80066
phattantran1997 <phattantran123@gmail.com>
2025-05-15 12:44:35 +1000

    add html pages

--- api/routes/base_routes.py
+++ api/routes/base_routes.py
@@ -16,9 +16,39 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 def home():
     return render_template('index.html')
+
+@main_bp.route('/bedroom')
+def bedroom():
+    return render_template('bedroom.html')
+
+@main_bp.route('/dining')
+def dining():
+    return render_template('dining.html')
+
+@main_bp.route('/living-room')
+def living_room():
+    return render_template('living-room.html')
+
+@main_bp.route('/kitchen')
+def kitchen():
+    return render_template('kitchen.html')
+
+@main_bp.route('/cleaning')
+def cleaning():
+    return render_template('cleaning.html')
+
+@main_bp.route('/bathroom')
+def bathroom():
+    return render_template('bathroom.html')
+
 @main_bp.route('/track-order')
 def track_order():
     return render_template('tracking.html')
+
 @main_bp.route('/cart')
 def cart():
     return render_template('cart.html')
+
+@main_bp.route('/product/<category>/<product>')
+def product_details(category, product):
+    return render_template('product_details.html', category=category, product=product)
